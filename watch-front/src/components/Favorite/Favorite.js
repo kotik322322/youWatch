a6fcd2a804cf1997c77e6471dd07b1cd58074d19
Kotik322 <kotan23111995@gmail.com>
2022-06-07 19:39:55 +0300

    added fnc removeFromFavorite, addToCart at Favorite cmp

--- watch-front/src/components/Favorite/Favorite.js
+++ watch-front/src/components/Favorite/Favorite.js
@@ -3,16 +3,35 @@ import styles from '../Favorite/Favorite.module.scss';
 import Button from '../Button/Button';
 import { GlobalState } from '../../GloabalState'
 
-import favoriteImage from "../../assets/watch.png";
-import favoriteImg from '../../assets/favorite/favoriteSolid.svg';
+import { AiFillHeart } from 'react-icons/ai'
 
 
 
-const Favorite = ({ name, price, imageUrl }) => {
+const Favorite = () => {
 
   const state = React.useContext(GlobalState)
   const [favorite, setFavorite] = state.favorite
-    console.log(favorite);
+  
+  const [cart, setCart] = state.cart
+
+    const removeFromFavorite = (product) => {
+      const result = favorite.filter(item => item._id !== product._id )
+      setFavorite(result)
+      localStorage.setItem("favorite", JSON.stringify(result))
+    }
+
+    const addToCart = (product) => {
+      const productExist = cart.find(item => item._id === product._id)
+      if (productExist) {
+          const result = cart.map(item => item._id === product._id ? { ...productExist, quantity: productExist.quantity + 1 } : item)
+          localStorage.setItem('cart', JSON.stringify(result))
+          setCart(JSON.parse(localStorage.getItem('cart')))
+      } else {
+          const result = [...cart, { ...product, quantity: 1 }]
+          localStorage.setItem('cart', JSON.stringify(result))
+          setCart(JSON.parse(localStorage.getItem('cart')))
+      }
+  }
 
 
 
@@ -28,15 +47,20 @@ const Favorite = ({ name, price, imageUrl }) => {
           favorite.length === 0
             ? <h4 className={styles.favoriteStatus}> Your favorite is empty </h4>
             : favorite.map(product => (
-              <div className={styles.favoriteInner}>
+              <div key={product._id} className={styles.favoriteInner}>
                 <img className={styles.favoriteImage} src={product.imageUrl[0]} alt="favorite_image" />
                 <p className={styles.favoriteName}>{product.name.length > 70 ?
             `${product.name.substring(0, 70)}...` : product.name
           }</p>
                 <p className={styles.favoritePrice}>$ {product.price}</p>
                 <div className={styles.favoriteFooter}>
-                  <img className={styles.favoriteIcon} src={favoriteImg} alt="favorite_icon_heart" />
+                  <AiFillHeart 
+                  className={styles.favoriteIcon} 
+                  style={{ fontSize: '24px', cursor: 'pointer' }}
+                  onClick={() => removeFromFavorite(product)}
+                  />
                   <Button
+                  onClick={() => addToCart(product)}
                     text={'Add to cart'}
                   />
                 </div>
